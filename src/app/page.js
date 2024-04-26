53d23f85569afdf54f41217d67f78dfed3dc500f
Thanus Kumaar <thanuskumaara@gmail.com>
2024-04-26 19:35:05 +0530

    Home page made responsive

--- src/app/page.js
+++ src/app/page.js
@@ -16,28 +16,28 @@ export default function Home() {
       <Cursor />
       <Navbar />
       <div className="flex-1 w-[98%] rounded-lg mb-5 bg-secondary-dark dark:bg-secondary-light mx-auto align-middle overflow-scroll noScrollBar">
-        <div className="flex flex-row justify-center px-[80px] py-[50px] h-full">
+        <div className="flex flex-col lg:flex-row justify-center p-5 lg:px-[80px] lg:py-[50px] lg:h-full">
           <div className="h-full w-fit">
             <Image
               src={"/Profile2.jpeg"}
-              className="rounded-l-lg float-end"
+              className="rounded-t-lg lg:rounded-t-lg lg:rounded-l-lg float-end"
               height={480}
               width={480}
             />
           </div>
-          <div className="h-full w-[800px] bg-primary-dark dark:bg-primary-light rounded-r-lg">
-            <div className="flex flex-col px-14 py-8 h-full justify-between">
-              <div className="flex flex-col gap-8 text-2xl">
+          <div className="h-full lg:w-[800px] bg-primary-dark dark:bg-primary-light rounded-b-lg lg:rounded-r-lg lg:rounded-b-0">
+            <div className="flex flex-col gap-6 lg:gap-0 px-8 lg:px-14 py-4 lg:py-8 h-full justify-between">
+              <div className="flex flex-col gap-2 lg:gap-8 text-lg lg:text-2xl">
                 <div>
                   I am,
                   <br />
-                  <span className="text-6xl underline font-bold underline-offset-8 decoration-2">
+                  <span className="text-4xl lg:text-6xl underline font-bold underline-offset-8 decoration-2">
                     Thanus Kumaar
                   </span>
                 </div>
-                <div className="text-xl font-light">
+                <div className="text-lg lg:text-xl font-light">
                   <span className="block mt-3">
-                    <span className="text-2xl font-semibold">
+                    <span className="text-lg lg:text-2xl font-semibold">
                       Full Stack developer | Student
                     </span>{" "}
                     <br></br>
@@ -48,14 +48,14 @@ export default function Home() {
                   </span>
                 </div>
               </div>
-              <div className="flex flex-col gap-6 w-[85%] mx-auto">
+              <div className="flex flex-col gap-6 w-[97%] lg:w-[85%] mx-auto">
                 <div className="flex flex-row hover:scale-[1.03] active:scale-[0.98] group transition duration-100">
                   <div className="h-12 w-12 rounded-l-md bg-secondary-dark dark:bg-secondary-light">
                     <IoIosDocument className="h-8 w-8 mx-auto mt-2" />
                   </div>
                   <div
                     className={
-                      "relative h-12 w-full bg-secondary-light rounded-r-md transition duration-100 text-primary-dark group-hover:text-white text-2xl text-center pt-[6px] before:content-[''] before:absolute before:rounded-r-md before:border-8 before:border-[#0f1c10] before:inset-0 before:bg-[#0f1c10] before:z-[-1] before:scale-x-0 before:transition before:duration-300 before:origin-left before:mix-blend-color-burn group-hover:before:z-0 group-hover:before:scale-x-100 cursor-buttons"
+                      "relative h-12 w-full bg-secondary-light rounded-r-md transition duration-100 text-primary-dark lg:group-hover:text-white text-2xl text-center pt-[6px] before:content-[''] before:hidden lg:before:block before:absolute before:rounded-r-md before:border-8 before:border-[#0f1c10] before:inset-0 before:bg-[#0f1c10] before:z-[-1] before:scale-x-0 before:transition before:duration-300 before:origin-left before:mix-blend-color-burn group-hover:before:z-0 group-hover:before:scale-x-100 cursor-buttons"
                     }
                   >
                     Curiculum Vitae
@@ -65,7 +65,7 @@ export default function Home() {
                 onClick={()=>window.location.href = '/projects'}>
                   <div
                     className={
-                      "relative h-12 w-full bg-secondary-light rounded-l-md transition duration-100 text-primary-dark group-hover:text-white text-2xl text-center pt-[6px] before:content-[''] before:absolute before:rounded-l-md before:border-8 before:border-[#0f1c10] before:inset-0 before:bg-[#0f1c10] before:z-[-1] before:scale-x-0 before:transition before:duration-300 before:origin-right before:mix-blend-color-burn group-hover:before:z-0 group-hover:before:scale-x-100 cursor-buttons"
+                      "relative h-12 w-full bg-secondary-light rounded-l-md transition duration-100 text-primary-dark lg:group-hover:text-white text-2xl text-center pt-[6px] before:content-[''] before:hidden lg:before:block before:absolute before:rounded-l-md before:border-8 before:border-[#0f1c10] before:inset-0 before:bg-[#0f1c10] before:z-[-1] before:scale-x-0 before:transition before:duration-300 before:origin-right before:mix-blend-color-burn group-hover:before:z-0 group-hover:before:scale-x-100 cursor-buttons"
                     }
                   >
                     Projects
